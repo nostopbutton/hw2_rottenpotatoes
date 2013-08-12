d42b3a0d5da6302b81f809fb4b05664afc85ff77
nostopbutton <pete@nostopbutton.com>
2013-08-11 17:07:34 -0700

    hw2

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,43 +6,7 @@ class MoviesController < ApplicationController
     # will render app/views/movies/show.<extension> by default
   end
 
-  # def store_params_in_session
-  #   store_ratings
-  #   store_sort
-
-  #   # flash.keep
-  #   # redirect_to movies_path @sort @ratings_filter
-
-  # end
-
-  # def store_sort
-  #   if params.has_key?(:sort)
-  #     @sort = session[:sort] = params[:sort]
-  #   elsif session.has_key?(:sort)
-  #     @sort = session[:sort]
-  #   # else 
-  #   #   @sort = session[:sort] = @all_ratings
-  #   end
-
-  # end
-
-  # def store_ratings
-  #   if params.has_key?(:ratings)
-  #     @ratings_filter = session[:ratings] = params[:ratings].keys
-  #   # elsif params.has_key?(:ratings_filter)
-  #   #   @ratings_filter = session[:ratings]= params[:ratings_filter]
-  #   elsif session.has_key?(:ratings)
-  #     @ratings_filter = session[:ratings]
-  #   else 
-  #     @ratings_filter = session[:ratings] = @all_ratings
-  #   end
-  # end
-
   def retrieve_params_from_session
-
-    # @sort = ""
-    # @ratings = Movie.all_ratings
-
 
     if params.has_key?(:sort)
       @sort = session[:sort] = params[:sort]
@@ -51,10 +15,6 @@ class MoviesController < ApplicationController
     else
       @sort = "no_sort"
     end
-
-    # if session.has_key?(:sort)
-    #   @sort = session[:sort]
-    # end
 
     if params.has_key?(:ratings)
       @ratings = session[:ratings] = params[:ratings]
@@ -69,10 +29,6 @@ class MoviesController < ApplicationController
     end
     @ratings_filter = @ratings.keys
 
-    # if session.has_key?(:ratings)
-    #   @ratings = session[:ratings]
-    # end
-
     flash.keep
     redirect_to movies_path(:sort => @sort, :ratings => @ratings)
 
@@ -85,34 +41,12 @@ class MoviesController < ApplicationController
 
     if params.has_key?(:sort)
       @sort = session[:sort] = params[:sort]
-    # elsif session.has_key?(:sort)
-    #   @sort = session[:sort]
-    # else
-    #   @sort = ""
     end
 
     if params.has_key?(:ratings)
       @ratings = session[:ratings] = params[:ratings]
       @ratings_filter = @ratings.keys
-    # elsif session.has_key?(:ratings)
-    #   @ratings = session[:ratings]
-    #   @ratings_filter = @ratings.keys
-    # else 
-    #   @ratings_filter = session[:ratings] = @all_ratings
     end
-
-    # retrieve_params_from_session unless params.has_key?(:sort) && params.has_key?(:ratings)
-
-    # @ratings_filter = @ratings.keys
-
-    # @sort = params[:sort]
-    # if params.has_key?(:ratings)
-    #   @ratings_filter = params[:ratings].keys
-    # elsif params.has_key?(:ratings_filter)
-    #   @ratings_filter = params[:ratings_filter]
-    # else 
-    #   @ratings_filter = @all_ratings
-    # end
 
     case @sort
     when "title"
